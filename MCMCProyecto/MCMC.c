f2f8dc5391d5aca986bbb8606f74c365b5fa1492
luis <luis@localhost.localdomain>
2018-12-09 20:45:45 -0600

    Final

--- MCMCProyecto/MCMC.c
+++ MCMCProyecto/MCMC.c
@@ -2,6 +2,41 @@
 #include <math.h>
 #include <stdlib.h>
 int main(int argc, char const *argv[]) {
-  
+	double PxIni,  Pxn=0, Pxn2=0, PxIni2, RANDM;
+	int delta;
+	int i,n=10,dif;
+  float MIni, bIni,Mx, bx;
+
+	FILE*R;
+  //Solicitamos los datos a evaluar
+	printf("Escribe la pendiente y la ordenada inicial\n");
+	scanf("%f",&MIni);
+	scanf("%f", &bIni);
+  // Esete dato nos dará las veces que se repetirá el metodo para encontrar el resultado que mejor se acople
+  printf("Número de evaluaciones del programa\n");
+	scanf("%i", &dif);
+	R=fopen("Respuesta","w");
+		for(i=0;i<dif;i++){
+//Numeros randoms para obtener las diferentes pendientes
+			Mx=MIni-(.1)+(.2)*(rand()/(double)RAND_MAX);
+			bx=bIni-(.1)+(.2)*(rand()/(double)RAND_MAX);
+      //ejecutamos el método de likelihoods
+  		PxIni=likelihood(MIni,bIni);
+			Pxn=likelihood(Mx,bx);
+  		Pxn2=likelihood(Mx,bx);
+  //    Conocer si aplica o no el valor para acercarce al valor correcto
+      if(PxIni<Pxn){
+			     MIni=Mx;
+			     bIni=bx;
+			}else{
+				RANDM= rand()/(double)RAND_MAX;
+				if (RANDM<Pxn2){
+				MIni=Mx;
+				bIni=bx;
+				}
+			}
+			fprintf(R,"%f %f \n", MIni, bIni);
+	}
+fclose(R);
   return 0;
 }
--- MCMCProyecto/func.c
+++ MCMCProyecto/func.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <math.h>
-int likelihood(); /*Se define el metodo de likelihood*/
-int likelihood(){
+#include <stdlib.h>
+float likelihood(float MIni,float bIni){
+	int i,j,n=10;
+	FILE*DT;
+	float r[10], r2, suma=0;
+	float *xAb, *yAb, *sigM, *sigb;
+//Manejo dinámico de la memoria
+	yAb=(float*)malloc(10*sizeof(float));
+	xAb=(float*)malloc(10*sizeof(float));
+	sigM=(float*)malloc(10*sizeof(float));
+	sigb=(float*)malloc(10*sizeof(float));
+//obtención de los datos de la investigación
+	DT = fopen("datos","r");
+		for (i=0;i<n;i++){
+			fscanf(DT,"%f", &xAb[i]);
+			fscanf(DT,"%f", &yAb[i]);
+			fscanf(DT,"%f", &sigM[i]);
+			fscanf(DT,"%f", &sigb[i]);
+		}
 
-return link;
+	fclose(DT);
+	for (i=0;i<10;i++){
+		//Logaritmo natural de la probabilidad
+		r[i]= yAb[i]-(xAb[i]*(MIni)+bIni);
+		r[i]= pow(r[i],2)-(sigM[i]*sigM[i]);
+		suma+=r[i];
+	}
+	suma*=-0.5;
+	r2= pow(2.718282,suma);
+	return r2;
 }
